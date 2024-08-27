ffc1b55fd2f74b3485a20610a18534c81443dd15
BJ-331 <bijayt090@gmail.com>
2024-08-27 15:53:22 +0545

    finished

---
+++ client/src/constant/url/url.ts
@@ -0,0 +1,10 @@
+export const Base_Url = "http://localhost:3333/";
+
+export const Auth_URL = "auth";
+export const USER_URL = "user";
+export const upload_Cloud_URL = "/cloudinary/upload";
+
+export const Room_URL = "room";
+
+export const Booking_URL = "book";
+export const REACT_APP_PUBLIC_FOLDER = "http://localhost:3333/pics/";
