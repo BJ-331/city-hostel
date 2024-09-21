709cc81df9773cbe1b693866a73bdeca08acbb7c
BJ <bijayt090@gmail.com>
2024-09-21 12:47:23 +0545

    Add files via upload

---
+++ client/src/constant/paths/profile-sidebar.ts
@@ -0,0 +1,20 @@
+export const ProfileSideBar = [
+  {
+    id: 1,
+    path: "",
+    title: "Manage Profile",
+    icon: "material-symbols-light:settings-outline",
+  },
+  {
+    id: 2,
+    path: "",
+    title: "Manage Bookings",
+    icon: "material-symbols-light:bookmarks-outline",
+  },
+  {
+    id: 3,
+    path: "",
+    title: "Current Users",
+    icon: "mdi:user-outline",
+  },
+];
