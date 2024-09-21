8ae003288137b03c640a0bcf8614eb88c829dfcc
BJ <bijayt090@gmail.com>
2024-09-21 20:37:39 +0545

    Update url.ts

--- client/src/constant/url/url.ts
+++ client/src/constant/url/url.ts
@@ -1,10 +1,10 @@
-export const Base_Url = "http://localhost:3333/";
-
+export const Base_Url = "https://city-hostel-server.vercel.app/";
+// export const Base_Url = "http://localhost:3333/";
 export const Auth_URL = "auth";
 export const USER_URL = "user";
 export const upload_Cloud_URL = "/cloudinary/upload";
 
 export const Room_URL = "room";
+export const PAYMENT_URL = "payment";
 
 export const Booking_URL = "book";
-export const REACT_APP_PUBLIC_FOLDER = "http://localhost:3333/pics/";
