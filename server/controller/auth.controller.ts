82bd23913ddd469240306a58b0c2b676739377bf
BJ <bijayt090@gmail.com>
2024-09-27 09:16:44 +0545

    Update auth.controller.ts

--- server/controller/auth.controller.ts
+++ server/controller/auth.controller.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { CreateEntity } from "../crud-operation/common-crud";
 import UserModel from "../model/user-model";
 import bcrypt from "bcrypt";
-import { generateToken } from "../utils";
+import { GenerateOtp, generateToken, sendMail } from "../utils";
 import CustomError from "../middleware/CusomError";
 import jwt from "jsonwebtoken";
+import { user } from "../types";
+
+const otpToSend = GenerateOtp().toString();
+
+interface customRequest extends Request {
+  user?: user;
+}
 
 export const createUser = async (
   req: Request,
@@ -18,19 +24,93 @@ export const createUser = async (
         const alreadyExists = await UserModel.findOne({
           email: userDetails.email,
         });
+
         if (alreadyExists) {
           return CustomError.entityAlreadyExistsError(next);
         }
         const hashedPwd = (await bcrypt.hash(req.body.password, 10)).toString();
+
         const newData = {
           ...userDetails,
           password: hashedPwd,
         };
-        CreateEntity(req, res, next, UserModel, newData);
+
+        const emailBody = ` <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
+          <h2 style="color: #4CAF50; text-align: center;">Your OTP Code</h2>
+          <p style="font-size: 16px; color: #333;">Hi <strong>${userDetails.userName}</strong>,</p>
+          <p style="font-size: 16px; color: #333;">Use the following OTP (One Time Password) to complete your sign-in process. This OTP is valid for the next 10 minutes.</p>
+          
+          <div style="text-align: center; margin: 20px 0;">
+            <p style="font-size: 24px; font-weight: bold; color: #000;">123456</p> ${otpToSend}
+          </div>
+          
+          <p style="font-size: 16px; color: #333;">If you did not request this, please ignore this email or contact support.</p>
+          <p style="font-size: 16px; color: #333;">Best Regards,</p>
+          <p style="font-size: 16px; color: #333;"><strong>Girls Hostel</strong></p>
+          <hr style="border-top: 1px solid #ddd; margin-top: 20px;">
+          <p style="font-size: 12px; text-align: center; color: #999;">&copy; 2024 Girl Hostel. All rights reserved.</p>
+        </div>
+        `;
+
+        await sendMail(userDetails.email, "Email Verification", emailBody);
+
+        const createuser = new UserModel(newData);
+        const user = await createuser.save();
+
+        const payload = {
+          userName: userDetails.userName,
+          email: userDetails.email,
+          contact: userDetails.contact,
+          address: userDetails.address,
+          password: userDetails.password,
+          role: userDetails.role,
+          _id: user._id,
+          otp: otpToSend,
+        };
+
+        const token = generateToken(payload);
+
+        res
+          .status(200)
+          .json({ msg: "Otp has been sent to you mail!!!", token: token });
       } else return CustomError.entityPropsMissingError(next);
     }
   } catch (error) {
     next(error);
+  }
+};
+
+export const verifyOTP = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { user } = req as customRequest;
+    const otp = req.body;
+
+    if (!otp) {
+      return CustomError.entityPropsMissingError(next);
+    }
+    if (otp === user?.otp) {
+      const userExists = await UserModel.findOne({
+        where: {
+          _id: user?._id,
+        },
+      });
+      if (!userExists) {
+        return CustomError.searchEntityMissingError(next);
+      }
+      const updateValue = {
+        ...userExists,
+        isVerify: true,
+      };
+      await userExists.updateOne(updateValue);
+      res.status(200).json({ msg: "verified!!!" });
+    }
+    return CustomError.invalidField(next);
+  } catch (error) {
+    return CustomError.tryCatchError(next);
   }
 };
 
