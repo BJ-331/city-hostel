f2e44846d5e6ba1162256cc0b41a87afedc1ff0e
BJ <bijayt090@gmail.com>
2024-09-27 15:00:08 +0545

    Update otp.controller.ts

--- server/controller/otp.controller.ts
+++ server/controller/otp.controller.ts
@@ -1,11 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { user } from "../types";
 import { GenerateOtp, generateToken, sendMail } from "../utils";
 import CustomError from "../middleware/CusomError";
-
-interface CustomRequest extends Request {
-  user: user;
-}
 
 export const SendOtp = async (
   req: Request,
@@ -13,21 +8,21 @@ export const SendOtp = async (
   next: NextFunction
 ) => {
   try {
-    const { user } = req as CustomRequest;
+    const data = req.body;
     const otp = GenerateOtp();
     const payload = {
-      userName: user.userName,
-      role: user.role,
-      email: user.email,
-      contact: user.contact,
-      address: user.address,
-      _id: user._id,
-      otp: user.otp,
+      userName: data.userName,
+      role: data.role,
+      email: data.email,
+      contact: data.contact,
+      address: data.address,
+      _id: data._id,
+      otp: data.otp,
     };
 
     const emailBody = ` <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
   <h2 style="color: #4CAF50; text-align: center;">Your OTP Code</h2>
-  <p style="font-size: 16px; color: #333;">Hi <strong>${user.userName}</strong>,</p>
+  <p style="font-size: 16px; color: #333;">Hi <strong>${data.userName}</strong>,</p>
   <p style="font-size: 16px; color: #333;">Use the following OTP (One Time Password) to complete your booking process.</p>
   
   <div style="text-align: center; margin: 20px 0;">
@@ -42,7 +37,7 @@ export const SendOtp = async (
 </div>
 `;
     const token = generateToken(payload);
-    await sendMail(user.email, "Email Verification", emailBody);
+    await sendMail(data.email, "Email Verification", emailBody);
     return res.status(200).json({
       msg: "OTP has been sent to your mail!!!",
       token: token,
