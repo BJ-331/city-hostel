7028a714be826c136cb2094b1f0768778e542692
BJ <bijayt090@gmail.com>
2024-09-27 08:14:06 +0545

    Create mailer.ts

---
+++ server/utils/mailer.ts
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+export const sendMail = async (to: string, subject: string, text: string) => {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent to ${to}`);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
