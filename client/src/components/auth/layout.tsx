7473d54e0279657c44e3d868b646735e2c118661
BJ <bijayt090@gmail.com>
2024-09-27 11:27:48 +0545

    Update layout.tsx

--- client/src/components/auth/layout.tsx
+++ client/src/components/auth/layout.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import { LogIn } from "./login";
 import { SignUp } from "./signup";
 import { useAuthContext } from "../../hooks";
+import { Otp } from "./otp";
 
 export const AuthLayout = React.memo(() => {
   const loginMenu = useAuthContext();
 
   return (
-    <div>{loginMenu?.authModalStatus.haveAccount ? <LogIn /> : <SignUp />}</div>
+    <div>
+      {loginMenu?.authModalStatus.haveAccount ? (
+        <LogIn />
+      ) : loginMenu?.authModalStatus.otpSection ? (
+        <Otp />
+      ) : (
+        <SignUp />
+      )}
+    </div>
   );
 });
