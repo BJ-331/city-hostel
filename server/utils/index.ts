e6e4a4e104fbaffd5b56c4e061f17cb4c61be39f
BJ <bijayt090@gmail.com>
2024-09-28 18:38:43 +0545

    Update index.ts

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,4 +1,4 @@
 export { generateToken, verifyTokens } from "./user-token";
 export { GenerateOtp } from "./generate-otp";
 export { sendMail } from "./mailer";
-export { BookingNotification } from "./create-email";
+export { BookingNotification, BookingRejected } from "./create-email";
