63d7ff6f6fa6b232f65112f0bb7675a6e3c82a71
BJ <bijayt090@gmail.com>
2024-09-28 19:01:36 +0545

    Update manage-booking.tsx

--- client/src/components/profile/manage-booking.tsx
+++ client/src/components/profile/manage-booking.tsx
@@ -57,6 +57,7 @@ export const ManageBookings = memo(() => {
     const newUpdatedValue = ownerHosteBookingDetails.filter(
       (data: bookingTypes) => data._id === _id
     );
+
     const updatedData = {
       ...newUpdatedValue[0],
       status: label,
@@ -99,58 +100,59 @@ export const ManageBookings = memo(() => {
           </tr>
         </thead>
 
-        {ownerHosteBookingDetails.map((detail: bookingTypes) => (
-          <tbody className="text-black  " key={detail._id}>
-            <tr className="py-4">
-              <td className="py-4 border text-center   p-4">
-                <InfoText title={detail.user.userName} />
-                <InfoText title={detail.user.email} />
-              </td>
-              <td className="py-4 border text-center  p-4">
-                {detail.user.contact}
-              </td>
-              <td className="py-4 border text-center  p-4">
-                {detail.user.address}
-              </td>
-              <td className="py-4 border text-center  p-4">
-                {detail.checkInDate.toString().slice(0, 10)}
-              </td>
-              <td className="py-4 border text-center  p-4">
-                {detail.createdAt.toString().slice(0, 10)}
-              </td>
-              <td
-                className="py-4 border text-center cursor-pointer  p-4"
-                onClick={() => nav(`/room-details/${detail.room._id}`)}
-              >
-                {detail.room.hostelName}
-              </td>
-              <td className="py-4 border text-center  p-4">
-                {detail.room.price}
-              </td>
-              <td className="py-4 border text-center  p-4">
-                {detail.paymentStatus}
-              </td>
-              <td className="py-4 border text-center  p-4">
-                <div className="flex flex-col gap-4  ">
-                  <button
-                    type="button"
-                    className="text-sm bg-love px-3 py-2 rounded-md text-other-white-100 font-semibold hover:animate-glow"
-                    onClick={() => makeDecision("cancelled", detail._id)}
-                  >
-                    Reject
-                  </button>
-                  <button
-                    type="submit"
-                    className="rounded-md bg-brand px-3 py-2 text-sm text-other-white-100 font-semibold hover:animate-glow"
-                    onClick={() => makeDecision("confirmed", detail._id)}
-                  >
-                    Accept
-                  </button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        ))}
+        {ownerHosteBookingDetails &&
+          ownerHosteBookingDetails?.map((detail: bookingTypes) => (
+            <tbody className="text-black  " key={detail._id}>
+              <tr className="py-4">
+                <td className="py-4 border text-center   p-4">
+                  <InfoText title={detail.user.userName} />
+                  <InfoText title={detail.user.email} />
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  {detail.user.contact}
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  {detail.user.address}
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  {detail.checkInDate.toString().slice(0, 10)}
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  {detail.createdAt.toString().slice(0, 10)}
+                </td>
+                <td
+                  className="py-4 border text-center cursor-pointer  p-4"
+                  onClick={() => nav(`/room-details/${detail.room._id}`)}
+                >
+                  {detail.room.hostelName}
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  {detail.room.price}
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  {detail.paymentStatus}
+                </td>
+                <td className="py-4 border text-center  p-4">
+                  <div className="flex flex-col gap-4  ">
+                    <button
+                      type="button"
+                      className="text-sm bg-love px-3 py-2 rounded-md text-other-white-100 font-semibold hover:animate-glow"
+                      onClick={() => makeDecision("cancelled", detail._id)}
+                    >
+                      Reject
+                    </button>
+                    <button
+                      type="submit"
+                      className="rounded-md bg-brand px-3 py-2 text-sm text-other-white-100 font-semibold hover:animate-glow"
+                      onClick={() => makeDecision("confirmed", detail._id)}
+                    >
+                      Accept
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          ))}
       </table>
     </div>
   );
